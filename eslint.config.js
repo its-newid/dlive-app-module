4c88d5516f9f44c560913c09ac691a5771c5414b
junsu-newid <135789016+junsu-newid@users.noreply.github.com>
2024-12-11 16:51:08 +0900

    🔧 eslint 설정파일 수정 및 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,55 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import * as tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-    { ignores: ['dist'] },
     {
-        extends: [js.configs.recommended, ...tseslint.configs.recommended],
-        files: ['**/*.{ts,tsx}'],
+        ignores: ['dist']
+    },
+    {
+        files: ['**/*.{ts,tsx,js,jsx}'],
         languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parser: tseslint.parser,
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            },
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
         },
         plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            react: reactPlugin,
             'react-hooks': reactHooks,
-            'react-refresh': reactRefresh
+            'react-refresh': reactRefresh,
+            prettier: prettierPlugin
         },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        },
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         rules: {
+            'prettier/prettier': 'error',
+            'react/prop-types': 'off',
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
-        },
-        linterOptions: {
-            reportUnusedDisableDirectives: true
+        }
+    },
+    {
+        files: ['*.js', '*.jsx'],
+        rules: {
+            '@typescript-eslint/explicit-function-return-type': 'off'
         }
     }
 );
