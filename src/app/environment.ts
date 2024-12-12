a70c4536921b37439ea496ea54d9cdb7b09c2fa2
junsu-newid <junsu@its-newid.com>
2024-12-12 15:34:11 +0900

    :wrench: 환경 변수 이름 vite에 맞게 변경

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,10 +1,10 @@
-const NODE_ENV = import.meta.env.NODE_ENV;
-const ENV_MODE = import.meta.env.ENVIRONMENT_NAME;
-const BASE_API_URL = import.meta.env.BASE_API_URL;
-const BEARER_TOKEN = import.meta.env.BEARER_TOKEN;
-const APP_VERSION = import.meta.env.VERSION;
-const CDN_URL = import.meta.env.CDN_URL;
-const AMPLITUDE_KEY = import.meta.env.AMPLITUDE_KEY;
+const NODE_ENV = import.meta.env.MODE;
+const ENV_MODE = import.meta.env.VITE_ENVIRONMENT_NAME;
+const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+const BEARER_TOKEN = import.meta.env.VITE_BEARER_TOKEN;
+const APP_VERSION = import.meta.env.VITE_VERSION;
+const CDN_URL = import.meta.env.VITE_CDN_URL;
+const AMPLITUDE_KEY = import.meta.env.VITE_AMPLITUDE_KEY;
 
 const CONTACT_EMAIL = 'app-marketing@its-newid.com';
 const ERROR_VIDEO_URL = CDN_URL + '/img/bgErrorBlack.mp4';
