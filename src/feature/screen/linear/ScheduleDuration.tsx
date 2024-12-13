169b15a8d2102a6a8bd9a2d3281ab9d546053fac
minho-newid <166360695+minho-newid@users.noreply.github.com>
2024-12-13 18:13:47 +0900

    🎨 Guide 컴포넌트 추가, 공통 컴포넌트 수정 (#11)

---
+++ src/feature/screen/linear/ScheduleDuration.tsx
@@ -0,0 +1,34 @@
+import { useMemo } from 'react';
+import styled from 'styled-components';
+import { TimeRange } from '@/component/ProgressBar';
+import { useTranslation } from 'react-i18next';
+
+type Props = {
+    range: TimeRange;
+};
+
+export function ScheduleDuration({ range }: Props) {
+    const { t, i18n } = useTranslation();
+
+    const getLocaleTime = (millis: number) => {
+        return t('time', {
+            val: new Date(millis),
+            formatParams: { val: { hour: 'numeric', minute: 'numeric', hour12: true } }
+        });
+    };
+
+    const times = useMemo(() => {
+        return {
+            start: getLocaleTime(range.start),
+            end: getLocaleTime(range.end)
+        };
+    }, [range, i18n.language]);
+
+    return <Duration>{`${times.start} - ${times.end}`}</Duration>;
+}
+
+const Duration = styled.span`
+    font: ${({ theme }) =>
+        `${theme.fonts.weight.normal} 24rem/32rem ${theme.fonts.family.pretendard}`};
+    color: ${({ theme }) => theme.colors.whiteAlpha89};
+`;
--- src/feature/screen/linear/guide/index.tsx
+++ src/feature/screen/linear/guide/index.tsx
@@ -1,5 +1,32 @@
-const Guide = () => {
-    return <h1>Guide</h1>;
-};
+import styled from 'styled-components';
+// import EpisodeInfo from './EpisodeInfo';
+// import Schedule from './Schedule';
+import { useAtomValue } from 'jotai';
+import { liveScreenOverlayState, LiveScreenOverlayType } from '@/atom/screen/linear';
+import EpisodeInfo from '@/feature/screen/linear/guide/EpisodeInfo';
 
+function Guide() {
+    const currentOverlay = useAtomValue(liveScreenOverlayState);
+
+    return (
+        currentOverlay === LiveScreenOverlayType.GUIDE && (
+            <Container>
+                <EpisodeInfo />
+                {/* <Schedule /> */}
+            </Container>
+        )
+    );
+}
 export default Guide;
+
+const Container = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.7);
+    z-index: 2;
+`;
