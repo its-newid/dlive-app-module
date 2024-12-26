2673b8bb49b2bb088c0bc3b24a3b0e6e2b1d8cb8
junsu-newid <junsu@its-newid.com>
2024-12-26 10:14:15 +0900

    :lipstick: styled-component에 &가 없던 로직들 수정

--- src/component/AgreeButton.tsx
+++ src/component/AgreeButton.tsx
@@ -26,10 +26,10 @@ const Container = styled(Clickable)`
     border-radius: 37rem;
     background-color: ${({ theme }) => theme.colors.grey10};
 
-    :hover {
+    &:hover {
         background-color: ${({ theme }) => theme.colors.main};
     }
-    :focus {
+    &:focus {
         background-color: ${({ theme }) => theme.colors.main};
     }
 
