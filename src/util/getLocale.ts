cf831394b7c64143e7e4867f5bacb7d32edf9b23
junsu-newid <junsu@its-newid.com>
2024-12-30 14:49:45 +0900

    :necktie: 웹뷰로부터 locale meta 받는 로직 추가

---
+++ src/util/getLocale.ts
@@ -0,0 +1,12 @@
+import { UserAgentLocale } from '@/type/userAgent.ts';
+
+export function getLocale() {
+    const metaContentName = document
+        .querySelector('meta[property="locale"]')
+        ?.getAttribute('content');
+    if (!metaContentName) return UserAgentLocale.DEFAULT;
+
+    const matchedLocale = metaContentName.toLowerCase();
+
+    return matchedLocale ?? UserAgentLocale.DEFAULT;
+}
